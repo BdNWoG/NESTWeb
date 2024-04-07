e75adb280d70a1f763517131374b293a1ec947ba
Billy Gao <billy.gao@hotmail.com>
2024-04-07 16:54:40 -0700

    afddasfdfa

--- nest-web/components/home.tsx
+++ nest-web/components/home.tsx
@@ -4,12 +4,11 @@
  */
 import Link from "next/link"
 
-export function home() {
+export function Landing() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-6 w-6" />
           <span className="sr-only">Acme Inc</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -191,25 +190,5 @@ export function home() {
         </nav>
       </footer>
     </div>
-  )
-}
-
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   )
 }
